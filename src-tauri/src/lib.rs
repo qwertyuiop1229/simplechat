4f3934eefabdefa43ce180c7e54c53eaec77f98c
qwe <hyuuga1229@gmail.com>
2026-04-29 22:42:39 +0900

    いろいろ

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,8 @@ pub fn run() {
       }
       Ok(())
     })
+    .plugin(tauri_plugin_notification::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
